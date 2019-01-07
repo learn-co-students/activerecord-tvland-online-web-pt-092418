5b3ff8853567f5fa4a402f84f0a1d61289e83e88
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-07 10:12:49 -0500

    Interim checkin

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 gem 'pry'
 gem "rspec"
+gem 'activerecord', '~>4.2'
 gem "sinatra-activerecord"
 gem "sqlite3"
 gem "rake"
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,10 +2,10 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 def full_name
-  
+  self.create
 end
 def list_roles
-
+  
 end
 
 end
